0d9e4d83bc189159510f5f052a2a27c3f16e6502
Nattarika Srichan <60070170@it.kmitl.ac.th>
2017-11-18 15:48:50 +0700

    add gragh crime type

---
+++ crime/crime type.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import pygal as pg
+from pygal.style import DarkStyle
+
+crimes = pd.read_csv('DATA/css.csv')
+lineread = pg.HorizontalBar(style=DarkStyle)
+lineread.title = ('Crimes Type')
+dct = {}
+for i in crimes['Primary Type']:
+    if i in dct:
+        dct[i] += 1
+    else:
+        dct[i] = 1
+for j in dct:
+    lineread.add(j, dct[j])
+lineread.render_to_file('C:/Users/Nattarika/Desktop/CrimesinChicago/crimemoremore.svg')
